748cc8c240e66fd69fb15e0c6b4cfbd584f48eaa
Andrew Madden <acmaddencs@gmail.com>
2017-02-07 00:13:17 -0500

    Dev shell updated to include preconfigured bootstrap and Firebase modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+];
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAtB2Ky1pQEHdC5k8L1oSERmcCrgC2DRjM',
+    authDomain: 'yourcampusneeds-3f6f3.firebaseapp.com',
+    databaseURL: 'https://yourcampusneeds-3f6f3.firebaseio.com',
+    storageBucket: 'yourcampusneeds-3f6f3.appspot.com',
+    messagingSenderId: '183814315744'
+};
+
+export const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
