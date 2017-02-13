e35f61e774d7839a9ef409f89428e9e86f8fc007
Andrew Madden <acmaddencs@gmail.com>
2017-02-13 17:48:00 -0500

    Holy shhhhhhhhh.... That is way too much configuration for one component unit test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,66 @@
-/* tslint:disable:no-unused-variable */
+import { TestBed } from '@angular/core/testing';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+import { CollapseDirective } from 'ng2-bootstrap';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { SquareComponent } from './square/square.component';
+import { PostComponent } from './post/post.component';
 
-describe('AppComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+describe('Component: AppComponent', () => {
+    let component: AppComponent;
+    beforeEach(() => {
+
+      const appRoutes: Routes = [
+        { path: 'square', component: SquareComponent },
+        { path: 'register', component: RegisterComponent },
+        { path: 'login', component: LoginComponent }
+      ];
+
+      const firebaseConfig = {
+        apiKey: 'AIzaSyAtB2Ky1pQEHdC5k8L1oSERmcCrgC2DRjM',
+        authDomain: 'yourcampusneeds-3f6f3.firebaseapp.com',
+        databaseURL: 'https://yourcampusneeds-3f6f3.firebaseio.com',
+        storageBucket: 'yourcampusneeds-3f6f3.appspot.com',
+        messagingSenderId: '183814315744'
+      };
+
+      const myFirebaseAuthConfig = {
+        provider: AuthProviders.Google,
+        method: AuthMethods.Password
+      };
+
+
+        TestBed.configureTestingModule({
+              declarations: [
+                AppComponent,
+                CollapseDirective,
+                RegisterComponent,
+                LoginComponent,
+                SquareComponent,
+                PostComponent
+              ],
+              imports: [
+                AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig),
+                BrowserModule,
+                FormsModule,
+                HttpModule,
+                RouterModule.forRoot(appRoutes, { useHash: true })
+              ],
+        });
+        const fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
     });
-    TestBed.compileComponents();
-  });
+
+    it('it should be defined', () => {
+        expect(component).toBeDefined();
+    });
 });
