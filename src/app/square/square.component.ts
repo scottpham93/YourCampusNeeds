34082ce707e699e84bea047f2909c3124a2d71df
Andrew Madden <acmaddencs@gmail.com>
2017-02-12 21:18:04 -0500

    Fixed the refresh bug on the square.

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnDestroy } from '@angular/core';
 import { AngularFire } from 'angularfire2';
 import { Router } from '@angular/router';
 import { AppComponent } from '.././app.component';
@@ -13,12 +13,6 @@ export class SquareComponent
   isActive: boolean;
   constructor(private af: AngularFire, private router: Router, private ac: AppComponent)
   {
-    // if the user isn't logged in then redirect them to the login page
-    this.af.auth.subscribe(auth => {
-      if(auth == null) { this.router.navigate(['/login']); }
-      else { this.ac.loginLogoutLink = 'Logout'; }
-    }).unsubscribe();
-
     this.isActive = true;
   }
 }
