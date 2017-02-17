4a928c2135bc751aa5a8ec546592c20b68e0e09f
Andrew Madden <acmaddencs@gmail.com>
2017-02-16 19:43:46 -0500

    Two way databinding in create post component.

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NewPostModel } from '../../models/new-post';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css']
 })
-export class CreatePostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class CreatePostComponent
+{
+  newPost: NewPostModel;
+  constructor()
+  {
+    this.newPost = new NewPostModel('', '', -1, '', '');
   }
 
+  post()
+  {
+    console.log(this.newPost.title);
+    console.log(this.newPost.description);
+    console.log(this.newPost.category);
+    console.log(this.newPost.reward);
+  }
 }
