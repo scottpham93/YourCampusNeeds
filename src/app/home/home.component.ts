d78cec7e9b45bb1a128ff9e2b2d56210f98313b8
cpd147 <cpd349@gmail.com>
2017-02-24 21:41:48 -0500

    home page not working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+import { CollapseDirective } from 'ng2-bootstrap';
+import { AngularFire, FirebaseAuthState } from 'angularfire2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit
+{
+  public isCollapsed: boolean;
+  public loginLogoutLink;
+  public authStatus: FirebaseAuthState;
 
-  constructor() { }
+  constructor(private af: AngularFire, private router: Router)
+  {
+    this.isCollapsed = true;
+  }
 
   ngOnInit() {
+  }
+
+  login()
+  {
+    this.isCollapsed = true;
+    this.router.navigate(['/login']);
+  }
+
+  register()
+  {
+    this.isCollapsed = true;
+    this.router.navigate(['/register']);
   }
 
 }
